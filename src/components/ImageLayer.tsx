cc82efaf562270d0bfe4fee9f655e2eb64fd16ab
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2025-11-29 10:06:26 +0100

    feat: rounded image corners

--- src/components/ImageLayer.tsx
+++ src/components/ImageLayer.tsx
@@ -1,4 +1,4 @@
-import { Image, useImage } from "@shopify/react-native-skia";
+import { Group, Image, Skia, useImage } from "@shopify/react-native-skia";
 import React from "react";
 
 type ImageLayerProps = {
@@ -7,14 +7,34 @@ type ImageLayerProps = {
   y: number;
   width: number;
   height: number;
+  borderRadius?: number;
 };
 
-export function ImageLayer({ uri, x, y, width, height }: ImageLayerProps) {
+const DEFAULT_BORDER_RADIUS = 18;
+
+export function ImageLayer({
+  uri,
+  x,
+  y,
+  width,
+  height,
+  borderRadius = DEFAULT_BORDER_RADIUS,
+}: ImageLayerProps) {
   const image = useImage(uri);
 
   if (!image) {
     return null;
   }
 
-  return <Image image={image} x={x} y={y} width={width} height={height} />;
+  const clip = Skia.RRectXY(
+    Skia.XYWHRect(x, y, width, height),
+    borderRadius,
+    borderRadius
+  );
+
+  return (
+    <Group clip={clip}>
+      <Image image={image} x={x} y={y} width={width} height={height} />
+    </Group>
+  );
 }
